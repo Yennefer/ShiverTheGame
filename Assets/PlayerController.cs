bb15928f029a8e195255a38e81bcefd21389ee10
Alena Kabadinova <zakomoldinaav@gmail.com>
2017-11-04 23:54:23 +0500

    Player's controller added

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 0.01f;
+
+	private Animator animator;
+
+	private void Awake() {
+		animator = GetComponent<Animator>();
+	}
+
+	private void FixedUpdate () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			animator.SetTrigger("GoUp");
+        	Move(new Vector2(0, 1));
+    	} else if (Input.GetKey(KeyCode.DownArrow)) {
+			animator.SetTrigger("GoDown");
+        	Move (new Vector2(0, -1));
+    	} else if (Input.GetKey(KeyCode.RightArrow)) {
+			animator.SetTrigger("GoRight");
+			Move (new Vector2(1, 0));
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			animator.SetTrigger("GoLeft");
+			Move (new Vector2(-1, 0));
+		}
+	}
+
+	private void Move(Vector2 direction) {
+		transform.Translate (direction * speed);
+	}
+}
